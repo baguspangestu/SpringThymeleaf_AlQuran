640571bb09d98fba8ad13edb60b31772da6b7976
Bagus Pangestu <baguspangestu@yandex.com>
2023-04-05 04:01:46 +0700

    Kelar, tinggal tafsir

--- src/main/resources/static/scripts/detail.js
+++
@@ -1,7 +0,0 @@
-const pilihAyat = document.querySelector("#pilih-ayat");
-
-pilihAyat.addEventListener("change", (event) => {
-  const ayats = document.querySelectorAll(".ayat");
-  const ayat = event.target.value;
-  ayats[ayat - 1].scrollIntoView({ block: "center" });
-});
